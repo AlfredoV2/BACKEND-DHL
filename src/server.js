d098cf32c2d7aa0afd9b5e51c6a3411efcc7be29
AlfredoV2 <adj.vargas98@gmail.com>
2021-12-18 00:01:56 -0500

    backend Dashboard(mensajero)

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const {rutasUsuario} = require("./controllers/usuarioController.js");
+const {rutasDashboard} = require("./controllers/mensajeroController.js"); 
 require("dotenv").config();
 
 app.use(cors()); //Middleware, permite que se conecten puertos públicos
@@ -11,6 +12,8 @@ app.use(express.json());//Middleware, convierte lo que manda usuario a JSON
 app.use("/usuario", rutasUsuario);//Recortar rutas
 //app.use("/envios", rutasEnvio)
 //agregar rutas
+
+app.use("/dashboard", rutasDashboard);   
 
 mongoose.connect(process.env.URL_database) //conexión a la BD
     .then(res => console.log("Conectado a DB"))
